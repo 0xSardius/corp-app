d05e6c2f1158a1877ad692566ef5383c0039535c
Sardius <0xsardius@gmail.com>
2023-11-28 19:51:26 -0500

    basic page linking done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <div>
+        <Link href="/performance">Performance</Link>
+      </div>
+      <div>
+        <Link href="/reliability">Reliability</Link>
+      </div>
+      <div>
+        <Link href="scale">Scale</Link>
+      </div>
+      Home Page
+    </div>
+  );
 }
